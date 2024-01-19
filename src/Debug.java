bb07373c36478046e63c045d2bd12b5591fc55d2
Svyatoslav Shypka <svyat200909@gmail.com>
2024-01-19 10:13:40 +0100

    task 3 is done

--- src/Debug.java
+++ src/Debug.java
@@ -3,7 +3,6 @@ import java.lang.reflect.Field;
 public class Debug {
 
     public static void fields(Object o) throws IllegalAccessException {
-        System.out.println("o.getClass() = " + o.getClass());
         for (Field x: o.getClass().getDeclaredFields()
              ) {
             x.setAccessible(true);
