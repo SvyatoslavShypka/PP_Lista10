4e744b9ec0ae5c42fc445900ddf05c5edac58b33
Svyatoslav Shypka <svyat200909@gmail.com>
2024-01-19 09:37:44 +0100

    task 3 is done

--- src/Debug.java
+++ src/Debug.java
@@ -1,5 +1,18 @@
+import java.lang.reflect.Field;
+
 public class Debug {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void fields(Object o) throws IllegalAccessException {
+        System.out.println("o.getClass() = " + o.getClass());
+        for (Field x: o.getClass().getDeclaredFields()
+             ) {
+            x.setAccessible(true);
+            System.out.println("Pole: " + x.getName() + " => " + x.getType() + ", " + x.get(o));
+        }
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Point point = new Point(3, 4);
+        Debug.fields(point);
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    private String a = "test";
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
